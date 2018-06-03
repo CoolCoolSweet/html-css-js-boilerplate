bf7dbd31d908af1057611f6fa8c1b92fb1a4c8da
Sean <38589300+CoolCoolSweet@users.noreply.github.com>
2018-06-03 10:15:42 -0400

    Initial commit

---
+++ gulpfile.js
@@ -0,0 +1,28 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const browserSync = require('browser-sync');
+const reload = browserSync.reload;
+
+gulp.task('styles', function () {
+    gulp.src('styles/**/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('./styles/'))
+        .pipe(reload({ stream: true }));
+});
+
+//Watch tasks
+
+gulp.task('bs', () => {
+    return browserSync.init({
+        server: {
+            baseDir: './'
+        }
+    });
+});
+
+
+
+gulp.task('default', ['bs', 'styles'], () => {
+    gulp.watch('./**/*.scss', ['styles']);
+    gulp.watch('./styles/styles.css', reload);
+});
